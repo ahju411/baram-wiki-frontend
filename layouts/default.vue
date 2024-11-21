22ad448582ee03373df71389a3ae550c0bffc680
hyesung <hshs321@naver.com>
2024-11-21 11:23:14 +0900

    hotfix : 광고 제거

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,39 +3,15 @@
 		<NuxtRouteAnnouncer />
 		<Header />
 		<main class="content">
-			<div class="ad-fixed left">
-				<client-only>
-					<Adsense 
-						v-if="showAds"
-						ad-slot="7398215734" 
-						:ad-style="{width: 160, height: 600}" 
-					/>
-				</client-only>
-			</div>
 			<NuxtPage />
-			<div class="ad-fixed right">
-				<client-only>
-					<Adsense 
-						v-if="showAds"
-						ad-slot="3458970727"
-						:ad-style="{width: 160, height: 600}" 
-					/>
-				</client-only>
-			</div>
 		</main>
 		<Footer />
 	</div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-
-const showAds = ref(false)
-
-onMounted(() => {
-	// 마운트 된 후에 광고 표시
-	showAds.value = true
-})
+import Header from '~/components/Header.vue';
+import Footer from '~/components/Footer.vue';
 </script>
 
 <style scoped lang="scss">
@@ -48,9 +24,8 @@ onMounted(() => {
 .content {
 	flex: 1;
 	margin: 0 auto;
-	max-width: calc(1200px + 320px);
+	max-width: 1200px;
 	padding: 0 20px;
-	position: relative;
 	display: flex;
 }
 </style>
