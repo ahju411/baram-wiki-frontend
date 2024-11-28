1860e288fd2303c9f68c3f4b161cf7e2541fc41c
hyesung <dongwon.seong@financialiq.co.kr>
2024-11-29 02:47:09 +0900

    test

--- app.vue
+++ app.vue
@@ -9,31 +9,15 @@ import { useRouter } from 'vue-router';
 
 const router = useRouter();
 
-let isNavigating = false;
-
 router.beforeEach((to, from, next) => {
-	// 이미 진행 중인 네비게이션이면 통과
-	if (isNavigating) {
-		isNavigating = false;
-		return next();
-	}
-
 	// 브라우저 네비게이션(뒤로가기/앞으로가기)인 경우
-	if (window.history.state?.forward) {
-		window.history.replaceState(null, '', to.fullPath);
+	if (window.history.state?.back || window.history.state?.forward) {
 		return next();
 	}
 
-	// 일반 네비게이션
-	isNavigating = true;
-	window.history.pushState({ forward: true }, '', to.fullPath);
+	// 모든 일반 네비게이션에 대해 강제 새로고침
 	window.location.href = to.fullPath;
 	return false;
-});
-
-// 네비게이션 완료 후 상태 초기화
-router.afterEach(() => {
-	isNavigating = false;
 });
 
 useHead({
