0ec6fcd9f2bb8bff24260f3e2dcec3eb53444074
hyesung <dongwon.seong@financialiq.co.kr>
2024-11-29 03:06:46 +0900

    reset

--- app.vue
+++ app.vue
@@ -5,23 +5,19 @@
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
-import { watch } from 'vue';
+import { useRouter } from 'vue-router';
 
-const route = useRoute();
+const router = useRouter();
 
-watch(
-	() => route.fullPath,
-	(newPath) => {
-		if (document.readyState === 'complete') {
-			window.location.reload();
-		}
-	}
-);
+// 라우트 변경 시 강제로 새로고침
+router.beforeEach((to) => {
+	window.location.href = to.fullPath; // 모든 라우트에서 강제 새로고침
+	return false;
+});
 
 useHead({
 	bodyAttrs: {
-		class: 'bg-layer-bg min-h-screen font-pretendard text-game-primary',
+		class: 'bg-layer-bg min-h-screen font-pretendard text-game-primary', // 원하는 Tailwind 클래스 추가
 	},
 });
 </script>
