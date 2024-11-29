8cee32c76937dd9237b72569caff1ff9565105e4
hyesung <dongwon.seong@financialiq.co.kr>
2024-11-29 10:21:49 +0900

    hotfix:[1.1.025] 최초 접속 시 세션 스토리지로 확인 후 새로고침

--- app.vue
+++ app.vue
@@ -9,17 +9,19 @@ import { useRouter } from 'vue-router';
 import { ref } from 'vue';
 
 const router = useRouter();
-const isFirstNavigation = ref(true);
+const isFirstVisit = ref(!sessionStorage.getItem('hasVisited'));
 
-// 라우트 변경 시 첫 번째 라우팅 이후에만 강제 새로고침
+// 라우트 변경 시 첫 방문이 아닐 때만 강제 새로고침
 router.beforeEach((to) => {
-	if (!isFirstNavigation.value) {
-		window.location.href = to.fullPath;
-		return false;
+	if (isFirstVisit.value) {
+		// 최초 방문 표시
+		sessionStorage.setItem('hasVisited', 'true');
+		isFirstVisit.value = false;
+		return true;
 	}
 
-	isFirstNavigation.value = false;
-	return true;
+	window.location.href = to.fullPath;
+	return false;
 });
 
 useHead({
