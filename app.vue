615a65999f2c69774173d9b1a3a5df6d6e026e16
hyesung <dongwon.seong@financialiq.co.kr>
2024-11-29 10:17:04 +0900

    hotfix:[1.1.024] 최초 접속시 새로고침 문제 해결

--- app.vue
+++ app.vue
@@ -6,18 +6,25 @@
 
 <script setup>
 import { useRouter } from 'vue-router';
+import { ref } from 'vue';
 
 const router = useRouter();
+const isFirstNavigation = ref(true);
 
-// 라우트 변경 시 강제로 새로고침
+// 라우트 변경 시 첫 번째 라우팅 이후에만 강제 새로고침
 router.beforeEach((to) => {
-	window.location.href = to.fullPath; // 모든 라우트에서 강제 새로고침
-	return false;
+	if (!isFirstNavigation.value) {
+		window.location.href = to.fullPath;
+		return false;
+	}
+
+	isFirstNavigation.value = false;
+	return true;
 });
 
 useHead({
 	bodyAttrs: {
-		class: 'bg-layer-bg min-h-screen font-pretendard text-game-primary', // 원하는 Tailwind 클래스 추가
+		class: 'bg-layer-bg min-h-screen font-pretendard text-game-primary',
 	},
 });
 </script>
